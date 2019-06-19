fa00bfde9730be1c5e5c281bedb4158b82e6b479
Anton <anton@dudarev.com>
2019-06-19 15:32:39 +0300

    add change

--- test/mfs/mail.php
+++ test/mfs/mail.php
@@ -1,10 +1,17 @@
 <?php
-$to = "dudarev.anton@gmail.com";
-$tema = "Форма обратной связи MFS";
-$message = "Ваше имя: ".$_POST['name']."<br>";
-$message .= "E-mail: ".$_POST['email']."<br>";
-$message .= "Сообщение: ".$_POST['message']."<br>";
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-mail($to, $tema, $message, $headers);
+if(isset($_POST['submit'])){
+$to = "dudarev.anton@gmail.com";; // Здесь нужно написать e-mail, куда будут приходить письма
+$from = $_POST['email']; // this is the sender's Email address
+$name = $_POST['name'];
+$subject = "Форма отправки сообщений с сайта";
+$message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$headers = "From:" . $from;
+mail($to,$subject,$message,$headers);
+}
 ?>
+
+<!--Переадресация на главную страницу сайта, через 3 секунды-->
+<script language="JavaScript" type="text/javascript">
+function changeurl(){eval(self.location="index.html");}
+window.setTimeout("changeurl();",1000);
+</script>
